66bfbec1a55ab6308262f2fc628600e25b2e9cd4
networkchano <136913430+networkchano@users.noreply.github.com>
2025-11-04 18:49:55 +0800

    button 2 add

--- Form1.vb
+++ Form1.vb
@@ -13,4 +13,16 @@ Public Class Form1
             MessageBox.Show("Error writing to file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim filePath As String = "sample.txt"
+            Using reader As New StreamReader(filePath) ' Comment
+                Dim content As String = reader.ReadToEnd()
+                MessageBox.Show(content, "File Content", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Error reading from file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 End Class
