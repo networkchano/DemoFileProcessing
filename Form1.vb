2340b127a6cf29fa441669c0bada2175209645c8
networkchano <136913430+networkchano@users.noreply.github.com>
2025-11-04 18:24:57 +0800

    Èvent Handler for StreamWriter, with pretty message box

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,16 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim filePath As String = "sample.txt"
+            Using writer As New StreamWriter(filePath)
+                writer.WriteLine("Hello, this is a test file!")
+                writer.WriteLine("Second line of text.")
+            End Using
+            MessageBox.Show("Data written successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            MessageBox.Show("Error writing to file: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 End Class
