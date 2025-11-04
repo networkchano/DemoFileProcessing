d197a54524fdc23c965e45d8defed6d22c495f74
networkchano <136913430+networkchano@users.noreply.github.com>
2025-11-04 18:45:43 +0800

    sir tapos na po

--- Form1.vb
+++ Form1.vb
@@ -4,7 +4,7 @@ Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
             Dim filePath As String = "sample.txt"
-            Using writer As New StreamWriter(filePath)
+            Using writer As New StreamWriter(filePath) ' Comment
                 writer.WriteLine("Hello, this is a test file!")
                 writer.WriteLine("Second line of text.")
             End Using
